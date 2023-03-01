c85ca23493c6b8dfe7cd4487b12e52b1aab2c1d3
holden1999 <gunawanholden15@gmail.com>
2023-03-01 18:35:01 +0700

    intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+//Create Element - 1
+const SayHello = () => {
+  return (
+      React.createElement("h1", {
+             class: "title"
+           }, "Hello")
+  )
+}
+
+// JSX (Javascript Extension)- 2
+const SayHello2 = () => {
+    const name = "Holden"
+  return <h1>Hello {name}</h1>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <SayHello />
+        <SayHello2 />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
